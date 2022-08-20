2fced0307b2204e883449cfbcfa45795fdaea20d
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-08-20 17:15:31 -0500

    started w2d4

--- week_2_day_3/exercise.js
+++ week_2_day_3/exercise.js
@@ -1,11 +1,18 @@
 //  Write a JavaScript statement that selects the #meDom ID element.
+document.getElementById("meDom").innerText = "#meDom ID";
 
 //  Write at least 2 ways to select the #myFamily ul element.
+document.getElementById("myFamily").innerText = "#myFamily ul";
+document.getElementsByTagName("ul").innerText = "#myFamily ul";
 
 //  Write a JavaScript statement that selects Hans.
+document.getElementById("myFamily").firstChild.nextSibling = "#hans";
 
 // Write a JavaScript statement that selects the .rideOrDie class.
+document.getElementsByClassName("rideOrDie").innerText = "#rideOrDie";
 
 //  Bonus: Can you select Brian Oconner?
+document.getElementById("myFamily").firstChild.nextSibling.nextSibling.innerText = "#Brian Oconner";
 
 //  Bonus2: Can you console log Brian Oconner?
+console.log(document.getElementById("myFamily").firstChild.nextSibling.nextSibling.innerText = "#Brian Oconner");
---
+++ week_2_day_4/practice.js
@@ -0,0 +1,9 @@
+//import your external javascript file into your html file
+
+// Change following elements using ONLY JavaScript and the DOM methods.
+
+// 1. Change the inner text of the first <h1> to “I’m using JavaScript!!”
+
+// 2. Change the background color of the first <div> with the id of animalContainer to blue
+
+// 3. Give all the <div> a black border
