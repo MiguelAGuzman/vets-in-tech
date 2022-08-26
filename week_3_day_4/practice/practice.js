d829c39892a4ab965e5312bf4f3db8d3f08a20f8
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-08-25 19:19:48 -0500

    week3 day4 p

---
+++ week_3_day_4/practice/practice.js
@@ -0,0 +1,9 @@
+/*
+TO DO APP
+
+1. EASY: First write out the function that will add on to the existing list from the users input.
+2. MEDIUM: Create a button at the bottom of the list to clear out the whole list.
+3. HARD: With each item that is added, also add a delete button with the ability to delete just that item. NOTE: you may need to look up how to access the li that the button is attached to and how to remove it.
+4. NIGHTMARE: Can you figure out a way to edit the task? NOTE: need to add an edit button
+*/
+
