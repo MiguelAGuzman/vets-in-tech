a231c4861131231c6780a24dcde88ef3ed1823ee
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-08-25 22:23:54 -0500

    week3 day4 p

--- week_3_day_4/practice/practice.js
+++ week_3_day_4/practice/practice.js
@@ -6,4 +6,28 @@ TO DO APP
 3. HARD: With each item that is added, also add a delete button with the ability to delete just that item. NOTE: you may need to look up how to access the li that the button is attached to and how to remove it.
 4. NIGHTMARE: Can you figure out a way to edit the task? NOTE: need to add an edit button
 */
+var task = document.getElementById('displayName');
 
+function addTask() {
+    var taskInput = task.value;
+    var newTask = document.createElement('li');
+newTask = taskInput;
+
+var deleteButton = document.createElement('button');
+deleteButton.setAttribute("class", "btn");
+deleteButton.appendChild(document.createTextNode('Delete'));
+
+var dateCol = document.getElementById('dateCol');
+dateCol.appendChild(newTask);
+// ul>li.appendChild(deleteButton).addEventListener('click', remove
+}
+
+function removeTasks() {
+    var dateCol = document.getElementById("dateCol");
+    dateCol.innerHTML = "";
+}
+
+// function removeSingle() {
+//     var dateCol = document.getElementById("dateCol");
+//     dateCol.removeChild(dateCol.childNodes[0]);
+// }
